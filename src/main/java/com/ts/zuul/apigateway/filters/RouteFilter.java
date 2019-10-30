e5871982d979e07b836d22ddd455f47e3d654fc1
Anjireddy.kata <anjireddy.kata@gmail.com>
2019-10-30 15:08:07 +0530

    added initial commit

---
+++ src/main/java/com/ts/zuul/apigateway/filters/RouteFilter.java
@@ -0,0 +1,35 @@
+package com.ts.zuul.apigateway.filters;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class RouteFilter extends ZuulFilter {
+    private static final Logger LOGGER = LoggerFactory.getLogger(RouteFilter.class);
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+        LOGGER.info(String.format("%s request to %s", request.getMethod(), request.getRequestURL().toString()));
+        return null;
+    }
+}
